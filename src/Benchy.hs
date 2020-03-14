687d3c80f5d62c9ca5bffb86bd556026af4c38f0
wdhg <wdhgrant@gmail.com>
2020-03-14 16:25:12 +0000

    add benchN

--- src/Benchy.hs
+++ src/Benchy.hs
@@ -5,16 +5,27 @@ import Data.Time.Clock.POSIX
 
 data Benchmark
   = Benchmark POSIXTime POSIXTime
+  | BenchmarkN [Benchmark]
 
 instance Show Benchmark where
-  show (Benchmark start end)
+  show benchmark@(Benchmark start end)
     =  "===== BENCHMARK ====="
-    ++ "\nSTART TIME: "
-    ++ (show start)
-    ++ "\n  END TIME: "
-    ++ (show end)
-    ++ "\nTIME TAKEN: "
-    ++ (show $ end - start)
+    ++ "\nSTART TIME: " ++ (show start)
+    ++ "\n  END TIME: " ++ (show end)
+    ++ "\nTIME TAKEN: " ++ (show $ timeTaken benchmark)
+  show benchmark@(BenchmarkN benchmarks)
+    =  "===== BENCHMARK ====="
+    ++ "\n[RAN " ++ (show $ length benchmarks) ++ " BENCHMARKS]"
+    ++ "\nMEAN TIME TAKEN: " ++ (show $ timeTaken benchmark)
+
+timeTaken :: Benchmark -> POSIXTime
+timeTaken (Benchmark start end)
+  = end - start
+timeTaken (BenchmarkN benchmarks)
+  = (sum times) / (fromIntegral $ length times)
+    where
+      times
+        = map timeTaken benchmarks
 
 bench :: a -> IO Benchmark
 bench value
@@ -23,3 +34,7 @@ bench value
     let !_ = value
     !end   <- getPOSIXTime
     return $ Benchmark start end
+
+benchN :: Int -> a -> IO Benchmark
+benchN n value
+  = BenchmarkN <$> (mapM bench $ replicate n value)
